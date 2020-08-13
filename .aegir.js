1de2742176f5fccc465395d8a9fde9550d86f055
achingbrain <alex@achingbrain.net>
2020-08-13 19:27:43 +0100

    chore: add note about which modules require node internals

--- .aegir.js
+++ .aegir.js
@@ -3,8 +3,10 @@
 module.exports = {
   webpack: {
     node: {
+      // needed by the ipfs-repo-migrations module
       path: true,
 
+      // needed by the abstract-leveldown module
       Buffer: true
     }
   }
