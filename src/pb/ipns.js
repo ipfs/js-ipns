7cad9611f87566047bc9e104be28970a877e9861
Vasco Santos <vasco.santos@moxy.studio>
2021-04-21 13:22:37 +0200

    fix: specify pbjs root (#119)

--- src/pb/ipns.js
+++ src/pb/ipns.js
@@ -7,7 +7,7 @@ var $protobuf = require("protobufjs/minimal");
 var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
 
 // Exported root namespace
-var $root = $protobuf.roots["default"] || ($protobuf.roots["default"] = {});
+var $root = $protobuf.roots["ipfs-ipns"] || ($protobuf.roots["ipfs-ipns"] = {});
 
 $root.IpnsEntry = (function() {
 
