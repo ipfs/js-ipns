063fc85f831a33ebcbda45e2a9526712cc327d8c
Alex Potsides <alex@achingbrain.net>
2021-07-09 17:49:33 +0100

    chore: update deps (#126)
    BREAKING CHANGE: uses new peer-id class and supporting ecosystem modules

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,10 +2,10 @@
 import ValidityType from './pb/ipns'
 
 export interface IPNSEntry {
-  value: Uint8Array, // value to be stored in the record
-  signature: Uint8Array, // signature of the record
-  validityType: ValidityType, // Type of validation being used
-  validity: Uint8Array, // expiration datetime for the record in RFC3339 format
+  value: Uint8Array // value to be stored in the record
+  signature: Uint8Array // signature of the record
+  validityType: ValidityType // Type of validation being used
+  validity: Uint8Array // expiration datetime for the record in RFC3339 format
   sequence: BigInt // number representing the version of the record
   ttl?: BigInt // ttl in nanoseconds
   pubKey?: Uint8Array
