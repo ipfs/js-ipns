a65d50a50a0168a6be3b00fd3b56e6716fbc5e98
achingbrain <alex@achingbrain.net>
2021-07-10 08:16:55 +0100

    chore: use primitive types instead of object types

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,8 +6,8 @@ export interface IPNSEntry {
   signature: Uint8Array // signature of the record
   validityType: ValidityType // Type of validation being used
   validity: Uint8Array // expiration datetime for the record in RFC3339 format
-  sequence: BigInt // number representing the version of the record
-  ttl?: BigInt // ttl in nanoseconds
+  sequence: bigint // number representing the version of the record
+  ttl?: bigint // ttl in nanoseconds
   pubKey?: Uint8Array
   signatureV2?: Uint8Array // the v2 signature of the record
   data?: Uint8Array // extensible data
